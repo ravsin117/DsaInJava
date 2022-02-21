19111d7fbd55ab05eb67b85c70113da2730c4b0d
Ravinder <ravsin117@gmail.com>
2022-02-21 18:20:42 +0530

    updated till multiply two LL

--- 3linkedlistlev2/linkedlist.java
+++ 3linkedlistlev2/linkedlist.java
@@ -1,11 +1,12 @@
 import java.util.List ;
 public class linkedlist {
-    public class ListNode {
-        int data = 0;
+
+    public static class ListNode {
+        int val = 0;
         ListNode next = null;
 
         ListNode(int val) {
-            this.data = val;
+            this.val = val;
         }
     }
 
@@ -28,7 +29,8 @@ public class linkedlist {
         if(l1==null || l2==null)
         return l1!=null?l1:l2;
 
-        ListNode dummy = new ListNode(-1), p = dummy , c1= l1,c2 = l2;
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy , c1= l1,c2 = l2;
         while(c1!=null&& c2!=null){
             if(c1.val<=c2.val){
                 p.next = c1;
@@ -60,7 +62,7 @@ public class linkedlist {
     // reverse node in k group
     private static ListNode th = null, tt = null;
 
-    private static void addFirstNode(ListNode node) {
+    private static void addFirstNode(ListNode node) {  
         if (th == null)
             th = tt = node;
         else {
@@ -111,6 +113,162 @@ public class linkedlist {
         ot.next = curr; // even if there still exist some nodes in list they will bw added in list
         return oh;
 
+    }
+
+    public static ListNode reverseInRange(ListNode head ,int n , int m ){
+        if(head == null || head.next ==null || n==m)
+        return head;
+        ListNode dummy = new ListNode(-1), prev = dummy , curr = head;
+        prev.next = head;
+        int i  = 1;
+        while(i<=m){
+            while(i>=n && i<=m){
+                ListNode forw = curr.next ;
+                curr.next = null;
+                addFirstNode(curr);
+                curr = forw;
+                i++;
+            }
+            if(i>m){
+                prev.next = th;
+                tt.next = curr;
+            }
+            i++;
+            prev = curr;
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+
+    //remove duplicates from linkedlist
+   /* 
+    // public static ListNode removeDupNode(ListNode head){
+    //     if(head ==null || head.next==null)
+    //     return head;
+    //     ListNode dummy = new ListNode(-101);
+    //     ListNode curr = head;
+    //     ListNode dp = dummy;
+    //     while(curr!=null){
+    //         if(curr.val!=dp.val){
+    //             dp.next = curr;
+    //             dp = curr;
+    //         }else{
+    //             dp.next=null;
+    //             ListNode forw = curr.next;
+    //             curr.next = null;
+    //             curr = forw;
+    //             continue;
+    //         }
+    //         curr = curr.next;
+    //     }
+    //     return dummy.next;
+
+    // }
+    */
+
+    //better code
+    public static ListNode removeDupNodes(ListNode head){
+        if(head==null || head.next==null)
+        return head;
+
+        ListNode dummy = new ListNode(-1),prev = dummy , curr = head;
+        while(curr!=null){
+            while(curr!=null && curr.val == prev.val){
+                ListNode forw = curr.next;
+                curr.next = null ;
+                curr = forw;
+            }
+            prev.next = curr;
+            if(curr!=null){
+                curr = curr.next;
+                prev = prev.next;
+            }  
+        }
+        return dummy.next;
+    }
+
+    // reverse a list
+     public static ListNode reverse(ListNode head){
+         if(head==null || head.next==null)
+         return head;
+        ListNode prev = null, curr = head;
+        while(curr!=null){
+            ListNode forw = curr.next;
+            curr.next = prev;
+            prev = curr ;
+            curr= forw;
+        } 
+        return prev;
+     }
+     // add two linkedlist version:pepcoding
+     public static ListNode addTwoNode(ListNode l1, ListNode l2){
+        if(l1==null || l2==null) 
+        return l1==null?l2:l1;
+        ListNode dummy = new ListNode(-1), prev = dummy;
+        ListNode c1 = reverse(l1) , c2 = reverse(l2) ;
+        int carry = 0 ;
+        while(c1!=null || c2!=null ||carry!=0){
+            int sum = carry+(c1!=null ?c1.val:0)+(c2!=null?c2.val:0);
+            int digit = sum%10;
+            carry = sum /10;
+            prev.next = new ListNode(digit);
+            prev = prev.next;
+            if(c1!=null) c1 = c1.next;
+            if(c2!=null) c2 = c2.next;             
+        }
+        return reverse(dummy.next);
+     }
+
+     // add two linkedlist version:leetcode 
+
+     // subtract two linkedlist
+    
+     // multiply two digits 
+    
+    public static ListNode multiplyTwoll(ListNode l1,  ListNode l2){
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
+         ListNode ans = new ListNode(-1), prev = ans;
+        while(l2!=null){
+            ListNode producthead = multiplydigits(l1,l2.val);
+            addList(prev,producthead);
+            prev = prev.next;
+            l2 = l2.next;
+        }
+        return reverse(ans.next);
+    }
+    public static ListNode multiplydigits(ListNode list , int d ){
+        ListNode dummy = new ListNode(-1), curr= list, prev = dummy;
+        int carry = 0; 
+        while(curr!=null || carry!=0){
+            int ans = carry + (curr!=null?curr.val:0)* d;
+            int digit = ans%10;
+            carry = ans/10;
+            prev.next = new ListNode(digit);
+            prev =prev.next;
+            if(curr!=null)
+            curr = curr.next;
+        }
+        return dummy.next; // new mutliplied list is created here
+    }
+
+    public static void addList(ListNode prev, ListNode list){
+        int carry =0 ;
+        
+        while(list!=null || carry!=0){
+            int sum = carry + (list!=null?list.val:0)+(prev.next!=null?prev.next.val:0);
+            int digit = sum % 10;
+            carry = sum/10;
+            if(prev.next!=null)
+            prev.next.val = digit;
+            else
+            prev.next = new ListNode(digit);
+
+            prev = prev.next;
+            if(list!=null)
+            list = list.next;
+        }
     }
 
 }
